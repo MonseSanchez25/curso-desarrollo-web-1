954b293140ed9324042201f1cab78107b9e4bc83
Monse <tsmonse6@gmail.com>
2022-12-18 16:49:05 -0600

    Se comenzo a conocer JavaScript y se realizo la tarea 5

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+let saborPastel = "Chocolate";
+const precioPastel = 200; 
+let Chocolate = true;
+let Vainilla = false; 
+
+console.log (saborPastel);
+console.log (precioPastel);
+
+
+
+
+let nombreUsuario = prompt("Hola bienvenido Yummy cake, proporcinanos tu nombre para poder atenderte");
+let UsCantidadPasteles = prompt("Cuantos pasteles deseas ordenar?");
+
+
+alert("Hola tu nombre es " + nombreUsuario + " " + "y vas a ordenar " +  UsCantidadPasteles + " " + "pasteles. En un momento te atenderemos ");
+alert (`Hola ${nombreUsuario}, vas a ordenar ${UsCantidadPasteles} pasteles.`);
+
+
+
+const pastelChocolate = 200
+const pastelVainilla = 150
+const suma = pastelChocolate + pastelVainilla;
+const multiplicación = pastelVainilla * pastelChocolate
+console.log(suma)
+console.log (resta)
+console.log(pastelVainilla == pastelChocolate)
+console.log(pastelVainilla < pastelChocolate)
+
+
+
+
